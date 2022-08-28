ee987089effd0dc6e6213a2f28b9cbcc8759cc41
Andrey <superlis2228@gmail.com>
2022-08-28 23:51:53 +0300

    Add more classes

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GitManual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+            int hello = 2;
+            for (int i = 0; i < hello; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
